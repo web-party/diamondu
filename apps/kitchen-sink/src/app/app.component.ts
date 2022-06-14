779023b4b06d2c9988b35435c059ca716ef893a4
Deimantas Bunys <dmn.wst@gmail.com>
2022-06-14 19:40:12 +0200

    fix code style issues

--- apps/kitchen-sink/src/app/app.component.ts
+++ apps/kitchen-sink/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'd-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'd-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
