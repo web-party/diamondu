ca0ccb978e4fffb233c178c8fb013f2ca2565159
Deimantas Bunys <dmn.wst@gmail.com>
2022-08-08 14:37:38 +0200

    implement sample bar chart

--- apps/kitchen-sink/src/app/compare-gh-users-stars/compare-gh-users-stars.component.ts
+++ apps/kitchen-sink/src/app/compare-gh-users-stars/compare-gh-users-stars.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
     selector: 'd-compare-gh-users-stars',
     templateUrl: './compare-gh-users-stars.component.html',
     styleUrls: ['./compare-gh-users-stars.component.css'],
 })
-export class CompareGhUsersStarsComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
+export class CompareGhUsersStarsComponent {
+    chartOptions: ChartConfiguration<'bar'>['options'] = { responsive: false };
+    chartData: ChartConfiguration<'bar'>['data'] = {
+        labels: ['user1', 'user2', 'user3'],
+        datasets: [{
+            data: [59, 88, 125]
+        }]
+    };
 }
