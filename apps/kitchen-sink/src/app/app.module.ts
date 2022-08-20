4f981b020c3b5a77275bfd6b0858474006ee4c40
Deimantas Bunys <dmn.wst@gmail.com>
2022-08-20 20:41:35 +0200

    get rid of the "Welcome" component

--- apps/kitchen-sink/src/app/app.module.ts
+++ apps/kitchen-sink/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { MagicTextComponent } from './magic-text/magic-text.component';
 import { CompareGhUsersStarsComponent } from './compare-gh-users-stars/compare-gh-users-stars.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NxWelcomeComponent,
         MagicTextComponent,
         CompareGhUsersStarsComponent,
     ],
