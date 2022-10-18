b3161931c387e0fdd1023ba608f699a3818be55d
Deimantas Bunys <dmn.wst@gmail.com>
2022-10-18 09:18:06 +0200

    create new app for testing material schematics

---
+++ apps/material-schematics-playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mat-sch-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+    title = 'material-schematics-playground';
+}
