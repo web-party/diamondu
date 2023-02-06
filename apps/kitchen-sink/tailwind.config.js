9028617fde7f5fdc71ffcd26200d76921e1e8e9e
Deimantas Wurster <deimantas.wurster@kyto.com>
2023-02-06 09:54:03 +0100

    reformat files

--- apps/kitchen-sink/tailwind.config.js
+++ apps/kitchen-sink/tailwind.config.js
@@ -2,12 +2,12 @@ const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
 
 module.exports = {
-  content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+        ...createGlobPatternsForDependencies(__dirname),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
