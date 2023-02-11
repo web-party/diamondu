b575bdbef23c37927ad049ff3706c0c8f40e16e4
Deimantas Wurster <deimantas.wurster@kyto.com>
2023-02-11 16:07:22 +0100

    install and enable `@mdi/font`

--- apps/vue/src/main.ts
+++ apps/vue/src/main.ts
@@ -2,10 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
-const vuetify = createVuetify({ components, directives });
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: { defaultTheme: 'dark' }
+});
 
 createApp(App).use(vuetify).mount('#app');
