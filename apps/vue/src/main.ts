de6b5f532ff45c6968f41a6a62333b1670cd1235
Deimantas Bunys <dmn.wst@gmail.com>
2023-02-25 15:41:21 +0100

    use Material v3

--- apps/vue/src/main.ts
+++ apps/vue/src/main.ts
@@ -6,11 +6,13 @@ import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { md3 } from 'vuetify/blueprints';
 
 const vuetify = createVuetify({
     components,
     directives,
-    theme: { defaultTheme: 'dark' }
+    theme: { defaultTheme: 'dark' },
+    blueprint: md3
 });
 
 createApp(App).use(vuetify).mount('#app');
