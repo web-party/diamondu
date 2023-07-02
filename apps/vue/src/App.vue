95f365d32a96c0a24721c1d3c1ddade2434e7ea9
Deimantas Bunys <dmn.wst@gmail.com>
2023-07-02 19:13:42 +0200

    fix(vue): improve html structure

--- apps/vue/src/App.vue
+++ apps/vue/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-    <AvatarStack />
-    <Countdown class="tw-block tw-m-3" />
-    <BankAccountDetails />
-    <Product />
+    <main class="tw-grid tw-gap-4 tw-grid-cols-1 md:tw-grid-cols-2">
+        <div>
+            <AvatarStack/>
+            <Countdown class="tw-block tw-m-4"/>
+            <BankAccountDetails/>
+            <CollectionLayoutSwitch class="tw-mt-4"/>
+        </div>
+    </main>
 </template>
 
 <script lang="ts" setup>
     import BankAccountDetails from './components/BankAccountDetails.vue';
-    import Product from './components/Product.vue';
     import AvatarStack from './components/AvatarStack.vue';
     import Countdown from './components/Countdown.vue';
+    import CollectionLayoutSwitch from './components/CollectionLayoutSwitch.vue';
 </script>
 
 <style>
