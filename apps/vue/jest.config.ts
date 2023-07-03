bcdbea4c14b1680ccfae9393bf5db7a9f149e2b9
Deimantas Bunys <dmn.wst@gmail.com>
2023-07-03 12:00:26 +0200

    chore: [nx migration] update-configs-jest-29

--- apps/vue/jest.config.ts
+++ apps/vue/jest.config.ts
@@ -5,15 +5,17 @@ module.exports = {
         '^.+.vue$': '@vue/vue3-jest',
         '.+.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
             'jest-transform-stub',
-        '^.+.tsx?$': 'ts-jest',
+        '^.+.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'apps/vue/tsconfig.spec.json',
+            },
+        ],
     },
     moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
     coverageDirectory: '../../coverage/apps/vue',
     snapshotSerializers: ['jest-serializer-vue'],
     globals: {
-        'ts-jest': {
-            tsconfig: 'apps/vue/tsconfig.spec.json',
-        },
         'vue-jest': {
             tsConfig: 'apps/vue/tsconfig.spec.json',
         },
