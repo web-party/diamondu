a77d9ca17b3eb25f4e84b33ca0b37a964b56519c
Deimantas Bunys <dmn.wst@gmail.com>
2023-07-03 12:46:58 +0200

    chore: [nx migration] update-16-0-0-migrate-7

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,15 @@
 import type { StorybookConfig } from '@storybook/core-common';
-
 export const rootMain: StorybookConfig = {
-    // webpackFinal: async (config, { configType }) => {
-    //   // Make whatever fine-grained changes you need that should apply to all storybook configs
-    //   // Return the altered config
-    //   return config;
-    // },
+  // webpackFinal: async (config, { configType }) => {
+  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+  //   // Return the altered config
+  //   return config;
+  // },
+};
+export const framework = {
+  name: '@storybook/angular',
+  options: {}
+};
+export const docs = {
+  autodocs: true
 };
