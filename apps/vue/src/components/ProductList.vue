d2876908fcb6e50967147b51c2a464b7b0e3041a
Deimantas Bunys <dmn.wst@gmail.com>
2023-07-21 19:14:01 +0200

    feat (vue): implement basic product card

--- apps/vue/src/components/ProductList.vue
+++ apps/vue/src/components/ProductList.vue
@@ -1,17 +1,21 @@
 <template>
-    <ul class="tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 md:tw-grid-cols-3 tw-gap-2">
+    <ul class="tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 md:tw-grid-cols-3 tw-gap-4">
         <li v-for="product of products" :key="product.id">
-            <v-skeleton-loader :loading="true" type="card">
-                <v-card></v-card>
+            <v-skeleton-loader :loading="loading" type="card">
+                <v-card>
+                    <v-img :src="product.imageUrl" :cover="true" />
+                    <v-card-title>{{ product.name }}</v-card-title>
+                    <v-card-text>{{ product.description }}</v-card-text>
+                </v-card>
             </v-skeleton-loader>
         </li>
     </ul>
 </template>
 
 <script lang="ts" setup>
-    // import Product from '../App.vue';
     import { faker } from '@faker-js/faker/locale/de';
     import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader';
+    import { ref } from 'vue';
 
     type Product = {
         id: number;
@@ -20,13 +24,17 @@
         description: string;
     };
 
-    const products = [...new Array(12)].map<Product>((_, i) => {
+    const loading = ref(true);
+
+    setTimeout(() => { loading.value = false }, 1000);
+
+    const products = ref([...new Array(12)].map<Product>((_, i) => {
         return {
             id: i,
             name: faker.commerce.product(),
             imageUrl: faker.image.urlLoremFlickr({ category: 'tech' }),
             description: faker.commerce.productDescription()
         };
-    });
+    }));
 
 </script>
