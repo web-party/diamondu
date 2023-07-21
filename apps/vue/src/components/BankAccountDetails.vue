4298b7933d0ad0f9bc28014f2cd545d7f0530a38
Deimantas Bunys <dmn.wst@gmail.com>
2023-07-21 20:03:38 +0200

    chore (vue): refactor component

--- apps/vue/src/components/BankAccountDetails.vue
+++ apps/vue/src/components/BankAccountDetails.vue
@@ -2,9 +2,9 @@
     <section class="tw-p-3 tw-border tw-border-dashed tw-border-purple-500 tw-max-w-lg tw-rounded-lg" aria-labelledby="bank-account-details">
         <h1 id="bank-account-details" class="tw-text-amber-300 tw-text-xl tw-font-semibold">Bank account details</h1>
         <dl>
-            <div>
+            <div class="tw-space-x-2">
                 <dt class="tw-inline-block tw-italic">IBAN:</dt>
-                <dd class="tw-ml-2 tw-inline-block">
+                <dd class="tw-inline-block">
                     {{ iban }}
                     <v-btn @click="copy(iban)" variant="plain" :icon="iconId" size="small" :disabled="copied" />
                 </dd>
