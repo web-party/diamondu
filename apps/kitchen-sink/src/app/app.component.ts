fc27934bfe1a46ed3da528f9493fad8ffae5954e
Deimantas Bunys <dmn.wst@gmail.com>
2023-12-09 14:05:54 +0100

    chore (kitchen-sink): convert entire app to standalone

--- apps/kitchen-sink/src/app/app.component.ts
+++ apps/kitchen-sink/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CompareGhUsersStarsComponent } from './compare-gh-users-stars/compare-gh-users-stars.component';
+import { MagicTextComponent } from './magic-text/magic-text.component';
 
 @Component({
     selector: 'd-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [MagicTextComponent, CompareGhUsersStarsComponent],
 })
 export class AppComponent {}
