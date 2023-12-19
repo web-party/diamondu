4ec521f757285196243dbbb4c5300f2a5f98fd8f
Deimantas Bunys <dmn.wst@gmail.com>
2023-12-19 21:58:24 +0100

    feat (vue-vite): now product can be liked

--- apps/vue-vite/src/app/components/ProductCollection.vue
+++ apps/vue-vite/src/app/components/ProductCollection.vue
@@ -7,8 +7,9 @@
         </template>
         <template v-else>
             <li v-for="product of products" :key="product.id">
-                <v-card class="tw-h-full">
-                    <v-img :src="product.imageUrl" cover />
+                <v-card class="tw-h-full tw-relative">
+                    <v-btn @click="product.liked = !product.liked" density="comfortable" color="purple" variant="text" :icon="product.liked ? 'mdi-heart' : 'mdi-heart-outline'" size="large" class="!tw-absolute tw-top-1 tw-right-1 tw-z-10" />
+                    <v-img :src="product.imageUrl" cover class="tw-opacity-60 tw-blur-[1px] hover:tw-opacity-100 hover:tw-blur-0" />
                     <v-card-title>{{ product.name }}</v-card-title>
                     <v-card-text>{{ product.description }}</v-card-text>
                 </v-card>
