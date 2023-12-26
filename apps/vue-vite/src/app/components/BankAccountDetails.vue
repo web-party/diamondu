086cbd9d8b211c13aaf055c97ca3343d41a8223b
Deimantas Bunys <dmn.wst@gmail.com>
2023-12-26 17:26:54 +0100

    feat (vue-vite): enhance component

--- apps/vue-vite/src/app/components/BankAccountDetails.vue
+++ apps/vue-vite/src/app/components/BankAccountDetails.vue
@@ -6,19 +6,31 @@
                 <dt class="tw-inline-block tw-italic">IBAN:</dt>
                 <dd class="tw-inline-block">
                     {{ iban }}
-                    <v-btn @click="copy(iban)" variant="plain" :icon="iconId" size="small" :disabled="copied" color="purple" />
+                    <v-btn @click="copyIban" icon variant="plain" size="small" :disabled="copied" color="purple">
+                        <v-icon :icon="iconId" />
+                        <v-tooltip v-model="tooltipActive" activator="parent">Copy to clipboard</v-tooltip>
+                    </v-btn>
                 </dd>
             </div>
         </dl>
+        <v-slide-y-transition>
+            <v-textarea v-if="text || copied" clearable label="Paste your clipboard contents here" rows="2" />
+        </v-slide-y-transition>
     </section>
 </template>
 
 <script setup lang="ts">
     import { faker } from '@faker-js/faker/locale/de';
     import { useClipboard } from '@vueuse/core';
-    import { computed } from 'vue';
+    import { computed, ref } from 'vue';
 
     const iban = faker.finance.iban(true, 'DE'),
-        { copy, copied } = useClipboard({ copiedDuring: 2000 }),
-        iconId = computed(() => `mdi-${copied.value ? 'check' : 'content-copy'}`);
+        { copy, copied, text } = useClipboard({ copiedDuring: 2000 }),
+        iconId = computed(() => `mdi-${copied.value ? 'check' : 'content-copy'}`),
+        tooltipActive = ref(false);
+
+    function copyIban() {
+        copy(iban);
+        tooltipActive.value = false;
+    }
 </script>
