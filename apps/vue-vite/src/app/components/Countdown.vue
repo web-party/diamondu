cf91b9882a23212fcb75920a614e3162c12ac0b0
Deimantas Bunys <dmn.wst@gmail.com>
2024-03-31 16:53:32 +0200

    refactor (vue-vite): streamline Countdown component

--- apps/vue-vite/src/app/components/Countdown.vue
+++ apps/vue-vite/src/app/components/Countdown.vue
@@ -1,27 +1,31 @@
 <template>
-    <time datetime="PT20M00S" aria-label="Remaining time" class="tw-px-2 tw-rounded tw-text-3xl tw-w-min" :class="{ ping: !finished }">
+    <!-- :key is important here as it ensures to replace the whole element every second for animation purposes -->
+    <time :key="displayedRemainingTime" :datetime="domRemainingTime" aria-label="Remaining time" class="tw-px-2 tw-rounded tw-text-3xl tw-w-min" :class="{ ping: !finished }">
         {{ displayedRemainingTime }}
     </time>
 </template>
 
 <script setup lang="ts">
     import { format, parse, subSeconds } from 'date-fns';
-    import { ref } from 'vue';
+    import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
 
     const props = withDefaults(defineProps<{ startAt?: string }>(), { startAt: '00:10' });
+    // TODO: consider using `dayjs` for better duration support
+    const remainingTime = ref(parse(props.startAt, 'mm:ss', Date.now()));
+    const displayedRemainingTime = computed(() => format(remainingTime.value, 'mm:ss'));
+    const domRemainingTime = computed(() => 'PT' + displayedRemainingTime.value.replace(':', 'M'));
+    // TODO: implement aria-live functionality by announcing e.g. every 5 seconds and when finished
+    const finished = computed(() => displayedRemainingTime.value === '00:00');
+    let intervalId: ReturnType<typeof setInterval>;
+    onMounted(() => {
+        intervalId = setInterval(() => {
+            remainingTime.value = subSeconds(remainingTime.value, 1);
+        }, 1000);
+    });
 
-    const displayedRemainingTime = ref(props.startAt);
-    const finished = ref(false);
-    // const remainingTime = ref('PT' + props.startAt.replace(':', 'M'));
-    const intervalId = setInterval(() => {
-        // TODO: consider using `dayjs` for better duration support
-        const parsed = parse(displayedRemainingTime.value, 'mm:ss', Date.now());
-        displayedRemainingTime.value = format(subSeconds(parsed, 1), 'mm:ss');
-        if (displayedRemainingTime.value === '00:00') {
-            finished.value = true;
-            clearInterval(intervalId);
-        }
-    }, 1000);
+    onUnmounted(() => { !finished.value && clearInterval(intervalId); })
+
+    watch(finished, newVal => { newVal && clearInterval(intervalId); });
 </script>
 
 <style scoped>
@@ -33,6 +37,6 @@
     }
 
     .ping {
-        animation: ping 1s cubic-bezier(0, 0, 0.2, 1) infinite;
+        animation: ping 1s cubic-bezier(0, 0, 0.2, 1);
     }
 </style>
