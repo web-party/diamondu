ec73ad4b6f5f52128b787420a13cd21a1c9e5472
Deimantas Bunys <dmn.wst@gmail.com>
2025-07-22 11:12:57 +0200

    chore(kitchen-sink): apply some refactoring

--- apps/kitchen-sink/src/app/hamburger-ingredients/hamburger-ingredients.component.ts
+++ apps/kitchen-sink/src/app/hamburger-ingredients/hamburger-ingredients.component.ts
@@ -1,18 +1,27 @@
-import { ChangeDetectionStrategy, Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    AfterViewInit,
+    ViewChild,
+    ViewChildren,
+    QueryList
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
     selector: 'd-hamburger-ingredients',
-    standalone: true,
     imports: [CommonModule],
     templateUrl: './hamburger-ingredients.component.html',
     styleUrl: './hamburger-ingredients.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HamburgerIngredientsComponent implements AfterViewInit {
-    @ViewChild('hero') private hero!: ElementRef;
+    @ViewChild('hero') private hero!: ElementRef<SVGElement>;
+    // images = viewChildren();
+    @ViewChildren(SVGImageElement) images!: QueryList<ElementRef<SVGImageElement>>;
 
     ngAfterViewInit(): void {
         gsap.registerPlugin(ScrollTrigger);
@@ -27,7 +36,8 @@ export class HamburgerIngredientsComponent implements AfterViewInit {
                 scrub: 1
             }
         })
-            .from(this.hero.nativeElement.querySelectorAll('image'), {
+            .from(this.images.toArray().map(el => el.nativeElement), {
+            //.from(this.hero.nativeElement.querySelectorAll('image'), {
                 y: (i) => [0, 830, 435, 500, 670, 290, 280, 350, 300, 420, 560, 350][i],
                 transformOrigin: '50%',
                 rotate: (i) => [0, -12, -9, 4, 10, -5, -8, -9, 9, 4, -5, 3][i]
