797ef71ab5b2fe5209bc0357be8c43d8b2706978
Deimantas Bunys <dmn.wst@gmail.com>
2025-07-23 18:06:59 +0200

    chore(mat-sch): run 'standalone bootstrap' migration (with some cleanup)

--- apps/material-schematics-playground/src/app/app.component.ts
+++ apps/material-schematics-playground/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GlobalNavComponent } from './global-nav/global-nav.component';
 
 @Component({
     selector: 'sch-root',
     template: `<sch-global-nav />`,
-    standalone: false
+    imports: [GlobalNavComponent]
 })
 export class AppComponent {
     title = 'material-schematics-playground';
--- apps/material-schematics-playground/src/main.ts
+++ apps/material-schematics-playground/src/main.ts
@@ -1,13 +1,25 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+    withEnabledBlockingInitialNavigation,
+    provideRouter,
+    withViewTransitions
+} from '@angular/router';
+import { appRoutes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
     enableProdMode();
 }
 
-platformBrowserDynamic()
-    .bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+        provideAnimations(),
+        provideRouter(appRoutes, withEnabledBlockingInitialNavigation(), withViewTransitions()),
+    ]
+})
     .catch((err) => console.error(err));
