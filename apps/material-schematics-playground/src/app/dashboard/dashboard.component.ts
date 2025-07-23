2c73fd44f6783c4606d831bace12e6a8c98ce998
Deimantas Bunys <dmn.wst@gmail.com>
2025-07-23 18:42:00 +0200

    chore(mat-sch): improve code format

--- apps/material-schematics-playground/src/app/dashboard/dashboard.component.ts
+++ apps/material-schematics-playground/src/app/dashboard/dashboard.component.ts
@@ -19,29 +19,44 @@ type Card = {
     selector: 'sch-dashboard',
     templateUrl: './dashboard.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [MatGridList, MatGridTile, MatCard, MatCardHeader, MatCardTitle, MatIconButton, MatMenuTrigger, MatIcon, MatMenu, MatMenuItem, MatCardContent, AsyncPipe]
+    imports: [
+        MatGridList,
+        MatGridTile,
+        MatCard,
+        MatCardHeader,
+        MatCardTitle,
+        MatIconButton,
+        MatMenuTrigger,
+        MatIcon,
+        MatMenu,
+        MatMenuItem,
+        MatCardContent,
+        AsyncPipe,
+    ],
 })
 export class DashboardComponent {
     @HostBinding('class') classes = 'tw-block tw-p-5';
 
     /** Based on the screen size, switch from standard to one column per row */
-    cards = inject(BreakpointObserver).observe(Breakpoints.Handset).pipe(
-        map(({ matches }): Card[] => {
-            if (matches) {
-                return [
-                    { title: 'Card 1', cols: 1, rows: 1 },
-                    { title: 'Card 2', cols: 1, rows: 1 },
-                    { title: 'Card 3', cols: 1, rows: 1 },
-                    { title: 'Card 4', cols: 1, rows: 1 }
-                ];
-            }
+    cards = inject(BreakpointObserver)
+        .observe(Breakpoints.Handset)
+        .pipe(
+            map(({ matches }): Card[] => {
+                if (matches) {
+                    return [
+                        { title: 'Card 1', cols: 1, rows: 1 },
+                        { title: 'Card 2', cols: 1, rows: 1 },
+                        { title: 'Card 3', cols: 1, rows: 1 },
+                        { title: 'Card 4', cols: 1, rows: 1 },
+                    ];
+                }
 
-            return [
-                { title: 'Card 1', cols: 2, rows: 1 },
-                { title: 'Card 2', cols: 1, rows: 1 },
-                { title: 'Card 3', cols: 1, rows: 2 },
-                { title: 'Card 4', cols: 1, rows: 1 }
-            ];
-        })
-    );
+                return [
+                    { title: 'Card 1', cols: 2, rows: 1 },
+                    { title: 'Card 2', cols: 1, rows: 1 },
+                    { title: 'Card 3', cols: 1, rows: 2 },
+                    { title: 'Card 4', cols: 1, rows: 1 },
+                ];
+            })
+        );
 }
