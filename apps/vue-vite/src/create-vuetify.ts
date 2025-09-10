6a2cc06e8ba5f75112cb36118f1ea03c8d948c69
Deimantas Bunys <dmn.wst@gmail.com>
2025-09-10 16:45:49 +0200

    chore(vue-vite): disable vuetify's style utility classes

--- apps/vue-vite/src/create-vuetify.ts
+++ apps/vue-vite/src/create-vuetify.ts
@@ -1,6 +1,5 @@
-import 'vuetify/styles';
+import './styles.scss';
 import '@mdi/font/css/materialdesignicons.css';
-import './styles.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
