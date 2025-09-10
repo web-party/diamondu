05a9818b329487c12b4bb67a5d860dbaba6a4432
Deimantas Bunys <dmn.wst@gmail.com>
2025-09-10 16:49:22 +0200

    chore(vue-vite): disable SB docs addon

--- apps/vue-vite/.storybook/main.ts
+++ apps/vue-vite/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-    addons: ['@chromatic-com/storybook', '@storybook/addon-docs'],
+    addons: ['@chromatic-com/storybook'],
 
     framework: {
         name: '@storybook/vue3-vite',
@@ -15,7 +15,3 @@ const config: StorybookConfig = {
 };
 
 export default config;
-
-// To customize your Vite configuration you can use the viteFinal field.
-// Check https://storybook.js.org/docs/react/builders/vite#configuration
-// and https://nx.dev/recipes/storybook/custom-builder-configs
