33fd07d8a8ecbbfc0c74fb033eafc5d1c7c604a6
Deimantas Bunys <dmn.wst@gmail.com>
2025-09-12 13:16:27 +0200

    fix(vue-vite): fix test

--- apps/vue-vite/src/app/App.spec.ts
+++ apps/vue-vite/src/app/App.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest';
-
 import { mount } from '@vue/test-utils';
 import App from './App.vue';
+import vuetify from '../create-vuetify';
 
 describe('App', () => {
     it('renders properly', () => {
-        const wrapper = mount(App, {});
-        expect(wrapper.text()).toContain('Welcome vue-vite 👋');
+        const wrapper = mount(App, { global: { plugins: [vuetify] }});
+        expect(wrapper.text()).toContain('IBAN:');
     });
 });
