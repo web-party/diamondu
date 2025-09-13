e64cdae73426f91e091c88bd7b41d4b9aee470b6
Deimantas Bunys <dmn.wst@gmail.com>
2025-09-13 15:19:59 +0200

    chore(nuxt): modernize ESLint config

--- apps/nuxt/eslint.config.mjs
+++ apps/nuxt/eslint.config.mjs
@@ -1,34 +1,13 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
-import baseConfig from '../../eslint.config.mjs';
+import withNuxt from './.nuxt/eslint.config.mjs'
+// import baseConfig from '../../eslint.config.mjs';
 
-const compat = new FlatCompat({
-    baseDirectory: dirname(fileURLToPath(import.meta.url)),
-    recommendedConfig: js.configs.recommended,
-});
-
-export default [
+export default withNuxt(
     {
-        ignores: ['**/dist'],
-    },
-    ...baseConfig,
-    ...compat.extends('@nuxt/eslint-config'),
-    {
-        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.vue'],
+        files: ['**/*.vue'],
         rules: {
             'vue/multi-word-component-names': 'off',
             'vue/html-indent': ['warn', 4],
             'vue/max-attributes-per-line': 'off',
         },
     },
-    {
-        ignores: [
-            '.nuxt/**',
-            '.output/**',
-            '**/vite.config.*.timestamp*',
-            '**/vitest.config.*.timestamp*',
-        ],
-    },
-];
+)/* .prepend(baseConfig) */;
--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -5,16 +5,19 @@ import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
     workspaceDir: '../../',
     srcDir: 'src',
+
     devServer: {
         host: 'localhost',
         port: 4200,
     },
+
     typescript: {
         typeCheck: true,
         tsConfig: {
             extends: './tsconfig.app.json',
         },
     },
+
     imports: {
         autoImport: true,
     },
@@ -25,5 +28,6 @@ export default defineNuxtConfig({
         plugins: [nxViteTsPaths()],
     },
 
-    compatibilityDate: '2024-08-03'
+    compatibilityDate: '2024-08-03',
+    modules: ['@nuxt/eslint'],
 });
