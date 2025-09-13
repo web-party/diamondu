29d4654ba95cd894db831dca3f41352f6ea7daa8
Deimantas Bunys <dmn.wst@gmail.com>
2025-09-13 17:17:18 +0200

    refactor(nuxt): modernize vitest config

--- apps/nuxt/vitest.config.ts
+++ apps/nuxt/vitest.config.ts
@@ -1,5 +1,4 @@
-/// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
