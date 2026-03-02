effe5b18cbc3e0da050376542265f1b1ad6cac20
Deimantas Bunys <dmn.wst@gmail.com>
2026-03-02 18:20:52 +0100

    refactor(vue-vite): simplify component template

--- apps/vue-vite/src/app/components/AdSkeleton.vue
+++ apps/vue-vite/src/app/components/AdSkeleton.vue
@@ -1,12 +1,9 @@
 <template>
-    <div class="relative mb-4 h-[250px] duration-500">
-        <div class="absolute h-full w-45">
-            <div class="shadow-3 overflow-hidden rounded-lg border border-sky-300/30">
-                <span v-for="(width, i) of itemWidths" :key="i" :style="`width: ${width}%`"
-                      class="block mx-2 mb-2 mt-3 h-5 animate-[pulse_1s_ease-in-out_infinite] bg-sky-300/30"
-                />
-            </div>
-        </div>
+    <div class="w-60 h-80 rounded-lg border border-sky-300/30">
+        <!-- :class="`w-[${width}%]`" wouldn't work because tw wouldn't see this dynamics -->
+        <span v-for="(width, i) of itemWidths" :key="i" :style="`width: ${width}%`"
+              class="block mx-2 mb-2 mt-3 h-5 motion-safe:animate-[pulse_1s_ease-in-out_infinite] bg-sky-300/30"
+        />
     </div>
 </template>
 
